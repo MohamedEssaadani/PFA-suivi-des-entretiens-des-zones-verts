1517328030066b5c9984582bca2460bc5612d25c
Mohamed Es-saadani <essaadani80@gmail.com>
2021-06-04 21:17:28 +0100

    'commit'

--- src/main/java/com/lazimessaadani/suivientretienszonesverts/repositories/TacheRepository.java
+++ src/main/java/com/lazimessaadani/suivientretienszonesverts/repositories/TacheRepository.java
@@ -1,9 +1,10 @@
 package com.lazimessaadani.suivientretienszonesverts.repositories;
 import com.lazimessaadani.suivientretienszonesverts.entities.Tache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-
+@CrossOrigin("*")
 public interface TacheRepository extends JpaRepository <Tache, Long>{
 
 }
