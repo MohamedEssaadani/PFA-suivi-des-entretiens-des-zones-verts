250a3ff526c4a02f0bc81a3a87fdbd57e4524459
Mohamed Es-saadani <essaadani80@gmail.com>
2021-06-27 19:22:29 +0100

    'commit'

--- src/main/java/com/lazimessaadani/suivientretienszonesverts/repositories/AffectationRepository.java
+++ src/main/java/com/lazimessaadani/suivientretienszonesverts/repositories/AffectationRepository.java
@@ -3,7 +3,9 @@ package com.lazimessaadani.suivientretienszonesverts.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lazimessaadani.suivientretienszonesverts.entities.Affectation;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 public interface AffectationRepository extends JpaRepository<Affectation, Long> {
 
 }
