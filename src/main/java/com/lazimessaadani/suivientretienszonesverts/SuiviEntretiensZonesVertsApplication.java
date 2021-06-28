477588e77ca25ea93f9bf0f51cf86c542c18c81d
Mohamed Es-saadani <essaadani80@gmail.com>
2021-06-28 10:18:53 +0100

    'commit'

--- src/main/java/com/lazimessaadani/suivientretienszonesverts/SuiviEntretiensZonesVertsApplication.java
+++ src/main/java/com/lazimessaadani/suivientretienszonesverts/SuiviEntretiensZonesVertsApplication.java
@@ -1,13 +1,18 @@
 package com.lazimessaadani.suivientretienszonesverts;
 
+import com.lazimessaadani.suivientretienszonesverts.entities.Personnel;
+import com.lazimessaadani.suivientretienszonesverts.entities.Tache;
 import com.lazimessaadani.suivientretienszonesverts.service.IDbInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class SuiviEntretiensZonesVertsApplication implements CommandLineRunner {
+public class SuiviEntretiensZonesVertsApplication implements CommandLineRunner, RepositoryRestConfigurer {
 
     @Autowired
     private IDbInitService dbInitService;
@@ -23,4 +28,12 @@ public class SuiviEntretiensZonesVertsApplication implements CommandLineRunner {
         dbInitService.initPersonnels();
         dbInitService.initTaches();
     }
+
+    //to return ids in json
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Personnel.class);
+        config.exposeIdsFor(Tache.class);
+    }
+
 }
